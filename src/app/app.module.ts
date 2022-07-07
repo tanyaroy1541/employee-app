1f91c13e14879eca43a1df79209ec618cdbb8a3a
tanyaroy1541 <tanyaroy1541@gmail.com>
2022-07-07 22:21:03 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { SearchemployeeComponent } from './searchemployee/searchemployee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddemployeeComponent,
+    SearchemployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
